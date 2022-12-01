3f0d53d7ffc5476c623c444ceb98076b0a019959
Viccky <tyagivivek528@gmail.com>
2022-12-01 12:42:24 +0530

    Created withdraw button, fund button, balance btn

--- front-end/simple-storage-dapp.js
+++ front-end/simple-storage-dapp.js
@@ -5,6 +5,10 @@ console.log("This is the dapp");
 const buttonSel = document.querySelector("#walletButton");
 const ethAmtSel = document.querySelector("#eth_amount");
 const fundBtnSel = document.querySelector("#fundBtn");
+const withBtnSel = document.querySelector("#withdrawBtn");
+// const withAmtSel = document.querySelector("#withdraw_amount");
+const balBtnSel = document.querySelector("#balanceBtn");
+
 
 //Wallet Connection...
 buttonSel.addEventListener("click", async () => {
@@ -26,8 +30,8 @@ buttonSel.addEventListener("click", async () => {
 //Fund Amount Function
 fundBtnSel.addEventListener("click", async () => {
   if (window.ethereum !== undefined) {
-    const amt = "0.5";
-    console.log(`Funding the program with ${amt} ethers`);
+    // const amt = "0.5";
+    console.log(`Funding the program with ${ethAmtSel.value} ethers`);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     console.log(signer);
@@ -35,23 +39,49 @@ fundBtnSel.addEventListener("click", async () => {
     console.log("signer has been acceptedd!!!");
     console.log(ethAmtSel.value);
     const transact = await contract.fund({
-      value: ethers.utils.parseEther(amt),
+      value: ethers.utils.parseEther(ethAmtSel.value),
     });
     console.log("Transaction successfull");
-    await listenForTransactionMined(transactionResponse, provider);
+    await listenForTransactionMined(transact, provider);
+    console.log("Done with listening...");
   } else {
     alert("You can't leave the Eth Field empty");
-    console.log("You can't leave the Eth Field empty");
+    console.log("You can't leave the Eth Field empty"); 
   }
 });
 
-//Listener
-function listenForTransactionMined(transactionResponse, provider) {
-  console.log(`Mining for the ${transactionResponse.hash}...`);
+//Withdraw function 
+withBtnSel.addEventListener("click",async()=>{
+  if(window.ethereum !== undefined){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = new provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+     
+}
+});
+
+
+//balanceChecker button
+balBtnSel.addEventListener("click",async ()=>{
+  if (window.ethereum !== undefined){
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const walletBal = await provider.getBalance(contractAddress);
+    console.log(`The balance of contract:${walletBal}`);
+  }
+  
+})
+//Listener (Tells about the txn confirmations...)
+function listenForTransactionMined(transact, provider) {
+  console.log(`Mining for the ${transact.hash}...`);
   // Here wait for the txn to be mined...
-  provider.once(transactionResponse.hash, (transactionReceipt) => {
-    console.log(
-      `Fulfilled with ${transactionReceipt.confirmations} confirmations...`
-    );
-  });
+  return new Promise((resolve,reject)=>{
+    provider.once(transact.hash, (transactionReceipt) => {
+      console.log(
+        `Fulfilled with ${transactionReceipt.confirmations} confirmations...`
+      );
+      resolve();
+    });
+   
+  })
 }
